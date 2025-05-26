34835dcd18ad228e258a4a4894afa5abe095832a
Vladimir Guerrero <104907260+vladasblood@users.noreply.github.com>
2025-05-26 23:11:05 +0800

    Update __init__.py

--- mmdet/models/data_preprocessors/__init__.py
+++ mmdet/models/data_preprocessors/__init__.py
@@ -5,9 +5,10 @@ from .data_preprocessor import (BatchFixedSizePad, BatchResize,
                                 MultiBranchDataPreprocessor)
 from .reid_data_preprocessor import ReIDDataPreprocessor
 from .track_data_preprocessor import TrackDataPreprocessor
+from .reppoints_data_preprocessor import RepPointsDataPreprocessor
 
 __all__ = [
     'DetDataPreprocessor', 'BatchSyncRandomResize', 'BatchFixedSizePad',
     'MultiBranchDataPreprocessor', 'BatchResize', 'BoxInstDataPreprocessor',
-    'TrackDataPreprocessor', 'ReIDDataPreprocessor'
+    'TrackDataPreprocessor', 'ReIDDataPreprocessor', 'RepPointsDataPreprocessor'
 ]
